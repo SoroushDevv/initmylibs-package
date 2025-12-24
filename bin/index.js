3ad72fcde2bd8554e83bb9effd9fd97cf9e27be9
soroushmoradi <soroushmoradi13.sm@gmail.com>
2025-12-24 21:00:23 +0000

    update logs

--- bin/index.js
+++ bin/index.js
@@ -75,6 +75,10 @@ const LIBRARIES = [
 
   if (toInstall.length === 0) {
     console.log(chalk.yellow("\nNo libraries selected for installation or update. Exiting."));
+      console.log("\nIf you have any feedback, suggestions, or issues, please share them with us:");
+  console.log(chalk.blue("GitHub: https://github.com/SoroushDevv"));
+  console.log(chalk.cyan("LinkedIn: https://www.linkedin.com/in/soroushmoradidev/"));
+  console.log("\nThank you for using this CLI!");
     return;
   }
 
